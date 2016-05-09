c257369da013152da13b21db2b51b5ba277bbb02
olehpidhi <olehpid@gmail.com>
2016-05-09 16:03:10 +0300

    Added repository for financial account.

--- FinancesManagment/Controllers/FinancialAccountController.cs
+++ FinancesManagment/Controllers/FinancialAccountController.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FinancesManagment.Models;
+using FinancesManagment.DAL;
 
 namespace FinancesManagment.Controllers
 {
     public class FinancialAccountController : Controller
     {
+
+        private IFinancialAccountRepository financialAccountsRepository;
+
+        public FinancialAccountController()
+        {
+            this.financialAccountsRepository = new FinancialAccountsRepository(ApplicationDbContext.Create());
+        }
         // GET: FinancialAccount
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string Name)
+        {
+            FinancialAccount newAccount = new FinancialAccount();
+            newAccount.Name = Name;
+            newAccount.Summary = 0;
+            financialAccountsRepository.AddFinancialAccount(newAccount);
+            financialAccountsRepository.Save();
+
         }
     }
 }
---
+++ FinancesManagment/DAL/IFinancialAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FinancesManagment.Models;
+
+namespace FinancesManagment.DAL
+{
+    public interface IFinancialAccountRepository: IDisposable
+    {
+        IEnumerable<FinancialAccount> GetFinancialAccounts();
+        FinancialAccount GetFinancialAccountById(int id);
+        void AddFinancialAccount(FinancialAccount account);
+        void DeleteFinancialAccount(int id);
+        void UpdateFinancialAccount(FinancialAccount account);
+        void Save();
+    }
+}
